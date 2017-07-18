c8af8a2bf6f0738673242c240aa0737effd311a6
Jack Tan <Jack.Tan@airnz.co.nz>
2017-07-18 13:57:59 +1200

    Added basic feed layout

--- src/layouts/feeds/feeds.js
+++ src/layouts/feeds/feeds.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { View, Text, Button, StatusBar } from "react-native";
 
+import styles from "./styles";
+
 const Feeds = ({ navigation }) => {
   const { navigate } = navigation;
 
   return (
-    <View>
-      <StatusBar backgroundColor="steelblue" barStyle="dark-content" />
-      <Text>Feeds</Text>
-      <Button
-        onPress={() => navigate("Login")}
-        title="Press to go to login page"
-      />
+    <View style={styles.container}>
+      {/*<View style={styles.feed}>
+        <Text>Update facebook</Text>
+      </View>
+      <View style={styles.feed}>
+        <Text>Update facebook</Text>
+      </View>*/}
+      <View style={styles.message}>
+        <Text>Start adding new friends to populate your feeds!</Text>
+      </View>
     </View>
   );
 };
