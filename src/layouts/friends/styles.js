64f4499666d1025a2200ae021c0389a851cbcb18
Jack Tan <Jack.Tan@airnz.co.nz>
2017-07-18 14:12:18 +1200

    Added basic friends layout

---
+++ src/layouts/friends/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column"
+  },
+  feed: {
+    borderBottomWidth: 2,
+    height: 200,
+    paddingTop: 20,
+    paddingLeft: 20
+  },
+  message: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingLeft: 20,
+    paddingRight: 20
+  }
+});
+
+export default styles;
