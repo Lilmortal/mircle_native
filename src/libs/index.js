1e1bfaea8031b6ad5189e476c8af8c83fb59779b
Jack Tan <Jack.Tan@airnz.co.nz>
2017-09-07 17:40:41 +1200

    User can now change their profile picture

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,5 +1,6 @@
+import * as camera from "./camera";
 import mapGenderValueToLabel from "./mapGenderValueToLabel";
 import pushNotification from "./pushNotification";
 import validate from "./validate";
 
-export { mapGenderValueToLabel, pushNotification, validate };
+export { camera, mapGenderValueToLabel, pushNotification, validate };
--- src/routes/profile/profile.js
+++ src/routes/profile/profile.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { View, Text, Image, FlatList, TouchableHighlight } from "react-native";
+import React, { Component } from "react";
+import { View, Text, Image, FlatList, TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 import { validate } from "../../libs";
 import { NUM_OF_COLUMNS } from "./constants";
 import { images } from "../../config";
+import Modal from "../../components/modal";
 import Picker from "../../components/picker";
 import DatePicker from "../../components/datePicker";
 import TextInput from "../../components/textInput";
@@ -13,74 +14,119 @@ import ProfileImage from "../../components/profileImage";
 import Biography from "./components/biography";
 import styles from "./styles";
 
-const checkIfTileIsInEdge = (id, size) => {
-  const style = {};
-
-  if (id % NUM_OF_COLUMNS === 0) {
-    style.borderRightWidth = 0;
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isProfilePictureModalOpen: false
+    };
   }
-  if (size.length - id < NUM_OF_COLUMNS) {
-    style.borderBottomWidth = 0;
-  }
-  return style;
-};
 
-const Profile = ({
-  emailAddress,
-  firstName,
-  surname,
-  gender,
-  phoneNumber,
-  birthDate,
-  occupation,
-  profileImage,
-  createdOn,
-  lastLoggedIn,
-  goToMap,
-  isAFriend,
-  friendId,
-  data
-}) => {
-  return (
-    <View style={styles.profile}>
-      <FlatList
-        style={styles.biographies}
-        numColumns={NUM_OF_COLUMNS}
-        ListHeaderComponent={
-          <Image
-            source={images.profileBackground}
-            style={styles.profileImageBackground}
-          >
-            <View style={styles.headerDescription}>
-              <ProfileImage source={profileImage} size={250} />
-              <Text style={styles.name}>
-                {firstName} {surname}
-              </Text>
-              <Text style={styles.email}>
-                {emailAddress}
-              </Text>
-            </View>
-            {isAFriend &&
-              <View style={styles.map}>
-                <TouchableHighlight onPress={goToMap}>
-                  <Icon name="map" size={25} />
-                </TouchableHighlight>
-              </View>}
-          </Image>
-        }
-        data={data}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) =>
-          <Biography
-            label={item.label}
-            value={item.value}
-            InputComponent={item.InputComponent}
-            disabled={item.disabled}
-            style={() => checkIfTileIsInEdge(item.id, data)}
-          />}
-      />
-    </View>
-  );
-};
+  setProfilePictureModal = isProfilePictureModalOpen => {
+    this.setState({
+      isProfilePictureModalOpen
+    });
+  };
+
+  checkIfTileIsInEdge = (id, size) => {
+    const style = {};
+
+    if (id % NUM_OF_COLUMNS === 0) {
+      style.borderRightWidth = 0;
+    }
+    if (size.length - id < NUM_OF_COLUMNS) {
+      style.borderBottomWidth = 0;
+    }
+    return style;
+  };
+
+  render() {
+    const {
+      emailAddress,
+      firstName,
+      surname,
+      profileImage,
+      goToMap,
+      isAFriend,
+      data,
+      getProfileImageFromMedium
+    } = this.props;
+    return (
+      <View style={styles.profile}>
+        <FlatList
+          style={styles.biographies}
+          numColumns={NUM_OF_COLUMNS}
+          ListHeaderComponent={
+            <Image
+              source={images.profileBackground}
+              style={styles.profileImageBackground}
+            >
+              <View style={styles.headerDescription}>
+                <TouchableOpacity
+                  onPress={() =>
+                    !isAFriend && this.setProfilePictureModal(true)}
+                >
+                  <View>
+                    <ProfileImage source={profileImage} size={250} />
+                  </View>
+                </TouchableOpacity>
+                <Text style={styles.name}>
+                  {firstName} {surname}
+                </Text>
+                <Text style={styles.email}>
+                  {emailAddress}
+                </Text>
+              </View>
+              {isAFriend &&
+                <View style={styles.map}>
+                  <TouchableOpacity onPress={goToMap}>
+                    <Icon name="map" size={25} />
+                  </TouchableOpacity>
+                </View>}
+            </Image>
+          }
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) =>
+            <Biography
+              label={item.label}
+              value={item.value}
+              InputComponent={item.InputComponent}
+              disabled={item.disabled}
+              style={() => this.checkIfTileIsInEdge(item.id, data)}
+            />}
+        />
+
+        <Modal
+          visible={this.state.isProfilePictureModalOpen}
+          onRequestClose={() => this.setProfilePictureModal(false)}
+          Icon={<Icon name="user" size={25} />}
+          title="Update Profile Picture"
+        >
+          <View>
+            <Button
+              onPress={async () => {
+                await getProfileImageFromMedium("camera");
+                this.setProfilePictureModal(false);
+              }}
+              style={styles.uploadButton}
+            >
+              <Text style={styles.buttonText}>Take a photo of yourself</Text>
+            </Button>
+            <Button
+              onPress={async () => {
+                await getProfileImageFromMedium("gallery");
+                this.setProfilePictureModal(false);
+              }}
+              style={styles.uploadButton}
+            >
+              <Text style={styles.buttonText}>Get it from your gallery</Text>
+            </Button>
+          </View>
+        </Modal>
+      </View>
+    );
+  }
+}
 
 export default Profile;
